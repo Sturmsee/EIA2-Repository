3629fae0dccf562a9cd010d6b9837527c45fa6ec
Sturmsee <nschiff98@web.de>
2022-10-11 17:29:43 +0200

    Aufgabe 01

---
+++ 01_Zufallsgedicht/scripts/zufallsgedicht.ts
@@ -0,0 +1,10 @@
+let subjektArray: string[] = ["Sandra", "Nicole", "Laura", "Siegbert", "Helmut"];
+let praedikatArray: string[] = ["malt", "zeigt", "reimt", "schreit", "kocht"];
+let objektArray: string[] = ["Obst", "Ersti-Tickets", "HFU-Karte", "Bücher", "Kuchen"];
+
+
+
+function ausgabe(): void {
+
+};
+
