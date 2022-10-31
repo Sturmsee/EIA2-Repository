77de3e3f23aef65daf0890a8c258703a9bdfda4c
Sturmsee <nschiff98@web.de>
2022-10-31 15:55:07 +0100

    Aufgabe 3

--- 03_Einkaufsliste/scripts/shopping.js
+++ 03_Einkaufsliste/scripts/shopping.js
@@ -1,43 +1,31 @@
-namespace shoppingList {
-
+"use strict";
+var shoppingList;
+(function (shoppingList) {
     //let editIconString: string = "https://img.icons8.com/cotton/64/000000/edit--v1.png";
     //let deleteIconString: string = "https://img.icons8.com/color/48/000000/delete.png";
-
     window.addEventListener("load", handleLoad);
-
-    let submitButton: HTMLElement = <HTMLInputElement>document.getElementById("addItemSubmit");
+    let submitButton = document.getElementById("addItemSubmit");
     //let createImg: HTMLImageElement = document.createElement("img");
     //let createItem: HTMLElement = document.createElement("li");
-
-    let deleteIcons: NodeListOf<HTMLElement> = document.querySelectorAll(".deleteIcon"); 
-    let editIcons: NodeListOf<HTMLElement> = document.querySelectorAll(".editIcon");
-
-    function handleLoad(_event: Event): void {
+    let deleteIcons = document.querySelectorAll(".deleteIcon");
+    let editIcons = document.querySelectorAll(".editIcon");
+    function handleLoad(_event) {
         submitButton.addEventListener("submit", addItem);
-
-        for(const icon of deleteIcons) {
+        for (const icon of deleteIcons) {
             icon.addEventListener("click", deleteItem);
         }
-
-        for(const icon of editIcons) {
+        for (const icon of editIcons) {
             icon.addEventListener("click", editItem);
         }
-
     }
-
-    function addItem(_event: Event): void {
+    function addItem(_event) {
         console.log("Item was added.");
-
     }
-
-    function deleteItem(_event: Event): void {
+    function deleteItem(_event) {
         console.log("Item will be deleted.");
     }
-
-    function editItem(_event: Event): void {
+    function editItem(_event) {
         console.log("Item can now be edited.");
-
     }
-
-
-}
+})(shoppingList || (shoppingList = {}));
+//# sourceMappingURL=shopping.js.map
