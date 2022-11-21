ddeae8fc989a506b2b88e41106905ef0a7904151
Sturmsee <nschiff98@web.de>
2022-11-21 14:08:06 +0100

    fixing of addItem

--- 06_DatabaseServer/Client/shopping.ts
+++ 06_DatabaseServer/Client/shopping.ts
@@ -6,7 +6,7 @@ namespace itemList06 {
     let form: HTMLFormElement;
 
     let databaseUrl: string = "https://webuser.hs-furtwangen.de/~schiffma/Database/?";
-    let collection: string = "&collection=Items";
+    //let collection: string = "&collection=Items";
     let data: Items[];
 
     let list: HTMLDivElement = <HTMLDivElement>document.getElementById("shoppingList");
@@ -19,7 +19,7 @@ namespace itemList06 {
 
         submitButton.addEventListener("click", addItem);
         requestData();
-        console.log("data received");
+        // console.log("data received");
         //console.log(setDate());
 
     }
@@ -27,27 +27,32 @@ namespace itemList06 {
     async function requestData(): Promise<void> {
 
         let _url: string = databaseUrl;
-        _url += "command=find" + collection;
+        //_url += "command=find" + collection;
+        let query: URLSearchParams = new URLSearchParams();
+        query.set("command", "find");
+        query.set("collection", "Items");
+        _url += query.toString();
         await communicate(_url);
         generateList();
-        form = <HTMLFormElement> document.getElementById("addItem");
+        form = <HTMLFormElement>document.getElementById("addItem");
     }
 
     function generateList(): void {
 
         let imgLink: string[] = [editIcon, deleteIcon];
-        console.log(data);
+        // console.log(data);
 
         for (let type in data) {
 
-            let item: Items = data[type];
- 
+            let items: Items = data[type];
+            console.log(items);
+
             let createItem: HTMLElement = document.createElement("span");
-            //console.log(item);
-            createItem.innerHTML = item.amount + "x " + item.name + ", Notes: " + item.notes;
+            
+            createItem.innerHTML = items.amount + "x " + items.name + ", Notes: " + items.notes;
 
             createItem.id = type.toString();
-            console.log(createItem.id);
+            //console.log(createItem.id);
             for (let icon of imgLink) {
                 let createImg: HTMLImageElement = document.createElement("img");
                 if (icon == editIcon) {
@@ -64,34 +69,48 @@ namespace itemList06 {
     }
 
 
+
     async function addItem(): Promise<void> {
 
         let url: string = databaseUrl;
         formData = new FormData(form);
         let json: FormDataJSON = {};
-        formData.append("date", setDate());
         console.log(formData);
 
-        for (let key in formData.keys()) {
-            if (!json[key]) {
-                let values: FormDataEntryValue[] = formData.getAll(key);
-                json[key] = values.length > 0 ? values : values[0];
-            }
-            //console.log(json[key]);
-        }
-        
+        formData.append("date", setDate());
+        // json = JSON.stringify(formData);
+        /* for (const pair of formData.entries()) {
+            console.log(typeof pair[1]);
+            const key: string = pair[0].toString();
+            const value: string = pair[1].toString();
+            json[key] = value;
+        } */
+
+        formData.forEach(function(value, key) {
+            json[key] = value;
+        });
+
+        // for (let key in formData.keys()) {
+        //     if (!json[key]) {
+        //         let values: FormDataEntryValue[] = formData.getAll(key);
+        //         json[key] = values.length > 0 ? values : values[0];
+        //     }
+        //     //console.log(json[key]);
+        // }
+
         let query: URLSearchParams = new URLSearchParams();
-        
-        
+
+
         console.log(json);
 
         query.set("command", "insert");
         query.set("collection", "Items");
         query.set("data", JSON.stringify(json));
         url += query.toString();
-        console.log(url);
+        // console.log(url);
         await communicate(url);
         alert("Item added!");
+        window.location.reload();
     }
 
     async function deleteItem(_event: Event): Promise<void> {
@@ -101,11 +120,17 @@ namespace itemList06 {
         let _id: string = targetParent.id;
         formData = new FormData(document.forms[0]);
         let _query: URLSearchParams = new URLSearchParams(<any>formData);
-        _query.append("id", _id);
+        console.log(_id);
 
-        url += "drop" + collection + "&data=" + _query.toString();
+        _query.set("command", "delete");
+        _query.set("collection", "Items");
+        _query.set("id", _id)
+
+        url += _query.toString();
         await communicate(url);
         alert("Item deleted!");
+
+        window.location.reload();
     }
 
     async function editItem(_event: Event,): Promise<void> {
@@ -116,10 +141,25 @@ namespace itemList06 {
         formData = new FormData(document.forms[0]);
         let _query: URLSearchParams = new URLSearchParams();
 
+        let json: FormDataJSON = {};
+        
+        formData.forEach(function(value, key) {
+            json[key] = value;
+        });
 
-        url += "update" + collection + "&id=" + _id + _query.toString();
+        _query.set("command", "update");
+        _query.set("collection", "Items");
+        _query.set("id", _id);
+        _query.set("data", JSON.stringify(json));
+
+        console.log(formData);
+        console.log(_query);
+
+        url +=  _query.toString();
+        console.log(url);
         await communicate(url);
         alert("Item updated!");
+        //window.location.reload();
     }
 
 
@@ -139,16 +179,14 @@ namespace itemList06 {
     async function communicate(_url: RequestInfo): Promise<void> {
 
         let response: Response = await fetch(_url);
-        let responseServer = await response.text();
+        let responseServer: string = await response.text();
         let responseJSON = JSON.parse(responseServer);
         data = responseJSON.data;
-        console.log("Response", response);
-        console.log(responseServer);
+        // console.log("Response", response);
+        // console.log(responseServer);
+        console.log(data);
 
     }
 
-        
-        
-    
 
 }
