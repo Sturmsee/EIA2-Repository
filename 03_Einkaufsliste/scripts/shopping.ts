560d7e77b7a8d3cd54c9c0718f2a9517d368d5f8
Sturmsee <nils.martin.stefan.schiffmann@hs-furtwangen.de>
2022-12-03 16:53:29 +0100

    added birbs

--- 03_Einkaufsliste/scripts/shopping.ts
+++ 03_Einkaufsliste/scripts/shopping.ts
@@ -3,6 +3,13 @@ namespace shoppingList {
     //let editIconString: string = "https://img.icons8.com/cotton/64/000000/edit--v1.png";
     //let deleteIconString: string = "https://img.icons8.com/color/48/000000/delete.png";
 
+    interface Item {
+        name: string;
+        amount: number;
+        notes: string;
+        date: string;
+    }
+
     window.addEventListener("load", handleLoad);
 
     let submitButton: HTMLElement = <HTMLInputElement>document.getElementById("addItemSubmit");
@@ -26,6 +33,12 @@ namespace shoppingList {
     }
 
     function addItem(_event: Event): void {
+        let AddItem: Item = {name: "", amount: 0, notes: "", date: ""};
+        AddItem.name = "";
+        AddItem.amount = 0;
+        AddItem.notes = "";
+        AddItem.date = "";
+
         console.log("Item was added.");
 
     }
@@ -39,5 +52,6 @@ namespace shoppingList {
 
     }
 
+    function dateToString(): string {}
 
 }
